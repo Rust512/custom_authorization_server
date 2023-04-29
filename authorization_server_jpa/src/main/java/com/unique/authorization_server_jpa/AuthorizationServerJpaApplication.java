6a15f23f2fb39650de85e501f5354053a36f2ffd
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2023-04-29 21:01:17 +0530

    initializing project

---
+++ authorization_server_jpa/src/main/java/com/unique/authorization_server_jpa/AuthorizationServerJpaApplication.java
@@ -0,0 +1,13 @@
+package com.unique.authorization_server_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerJpaApplication.class, args);
+	}
+
+}
