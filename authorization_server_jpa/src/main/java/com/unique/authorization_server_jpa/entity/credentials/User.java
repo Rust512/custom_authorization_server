500027588c3c72a224902c182e1961ca01ffbd7c
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2023-06-10 19:51:03 +0530

    Added to User Entity

---
+++ authorization_server_jpa/src/main/java/com/unique/authorization_server_jpa/entity/credentials/User.java
@@ -0,0 +1,32 @@
+package com.unique.authorization_server_jpa.entity.credentials;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+}
