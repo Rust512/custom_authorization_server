c289590e5250386350a82e43b4719c308d78e433
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2023-06-10 13:38:00 +0530

    Added the ClientTokenSettings Entity

---
+++ authorization_server_jpa/src/main/java/com/unique/authorization_server_jpa/entity/authorization_server/ClientTokenSettings.java
@@ -0,0 +1,38 @@
+package com.unique.authorization_server_jpa.entity.authorization_server;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "token_settings")
+public class ClientTokenSettings {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
+
+    @Column(name = "time_to_live_in_hours")
+    private Integer timeToLiveInHours;
+
+    @Column(name = "format")
+    private String tokenFormat;
+
+    @OneToOne
+    @JoinColumn(name = "client_id", referencedColumnName = "id")
+    private Client client;
+}
