a6d64c7cfe0510a44d1d157ff0000d99b60f0e1b
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2023-06-10 13:40:43 +0530

    Added all OneToOne and ManyToMany relationships to the Client Entity

--- authorization_server_jpa/src/main/java/com/unique/authorization_server_jpa/entity/authorization_server/Client.java
+++ authorization_server_jpa/src/main/java/com/unique/authorization_server_jpa/entity/authorization_server/Client.java
@@ -4,12 +4,20 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.EAGER;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Data
@@ -32,4 +40,39 @@ public class Client {
 
     @Column(name = "client_secret")
     private String clientSecret;
+
+    @ManyToMany(fetch = EAGER)
+    @JoinTable(
+            name = "client_authentication_method_mapping",
+            joinColumns = {@JoinColumn(name = "client_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "authentication_method_id", referencedColumnName = "id")}
+    )
+    private List<AuthenticationMethod> authenticationMethods = new ArrayList<>();
+
+    @ManyToMany(fetch = EAGER)
+    @JoinTable(
+            name = "client_grant_type_mapping",
+            joinColumns = {@JoinColumn(name = "client_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "grant_type_id", referencedColumnName = "id")}
+    )
+    private List<GrantType> grantTypes = new ArrayList<>();
+
+    @ManyToMany(fetch = EAGER)
+    @JoinTable(
+            name = "client_scope_map",
+            joinColumns = {@JoinColumn(name = "client_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "scope_id", referencedColumnName = "id")}
+    )
+    private List<Scope> scopes = new ArrayList<>();
+
+    @ManyToMany(fetch = EAGER)
+    @JoinTable(
+            name = "client_redirect_uri_map",
+            joinColumns = {@JoinColumn(name = "client_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "redirect_uri_id", referencedColumnName = "id")}
+    )
+    private List<RedirectURI> redirectURIs = new ArrayList<>();
+
+    @OneToOne(mappedBy = "client")
+    private ClientTokenSettings clientTokenSettings;
 }
