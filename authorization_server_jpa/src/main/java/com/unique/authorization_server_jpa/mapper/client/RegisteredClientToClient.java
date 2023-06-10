e55a1c0b438e344b80233296da66b8ca6147e63d
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2023-06-10 16:51:52 +0530

    Added a mapper for converting ClientAuthenticationMethod object to AuthenticationMethod object and using it to further complete the mapper for converting RegisteredClient object to Client object

--- authorization_server_jpa/src/main/java/com/unique/authorization_server_jpa/mapper/client/RegisteredClientToClient.java
+++ authorization_server_jpa/src/main/java/com/unique/authorization_server_jpa/mapper/client/RegisteredClientToClient.java
@@ -1,13 +1,19 @@
 package com.unique.authorization_server_jpa.mapper.client;
 
 import com.unique.authorization_server_jpa.entity.authorization_server.Client;
+import com.unique.authorization_server_jpa.mapper.authentication_method.ClientAuthenticationMethodToAuthenticationMethod;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
 @Component
+@RequiredArgsConstructor
 public class RegisteredClientToClient implements Function<RegisteredClient, Client> {
+
+    private final ClientAuthenticationMethodToAuthenticationMethod clientAuthMethodToAuthMethod;
+
     @Override
     public Client apply(RegisteredClient registeredClient) {
         return Client.builder()
@@ -15,6 +21,10 @@ public class RegisteredClientToClient implements Function<RegisteredClient, Clie
                 .clientId(registeredClient.getClientId())
                 .clientName(registeredClient.getClientName())
                 .clientSecret(registeredClient.getClientSecret())
+                .authenticationMethods(registeredClient.getClientAuthenticationMethods()
+                        .stream()
+                        .map(clientAuthMethodToAuthMethod)
+                        .toList())
                 .build();
     }
 }
