2119030bce078a614dd76bb4efad3cd8fe2dcb6e
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2023-06-10 19:57:08 +0530

    Added a List of Authorities to the user

--- authorization_server_jpa/src/main/java/com/unique/authorization_server_jpa/entity/credentials/User.java
+++ authorization_server_jpa/src/main/java/com/unique/authorization_server_jpa/entity/credentials/User.java
@@ -4,12 +4,19 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.EAGER;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Data
@@ -29,4 +36,12 @@ public class User {
 
     @Column(name = "password")
     private String password;
+
+    @ManyToMany(fetch = EAGER)
+    @JoinTable(
+            name = "user_role_map",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")}
+    )
+    List<Authority> authorities = new ArrayList<>();
 }
