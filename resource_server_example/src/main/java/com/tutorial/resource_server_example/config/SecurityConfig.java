010027a83d07dc1dc5d70832089a6c49e4110f42
Devang Hemant Bhagwat <dbhagwat512@gmail.com>
2023-06-14 17:18:51 +0530

    Added security configuration

---
+++ resource_server_example/src/main/java/com/tutorial/resource_server_example/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.tutorial.resource_server_example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Resource server configuration
+        http.oauth2ResourceServer(
+                resourceServer -> resourceServer.jwt(
+                        jwtConfig -> jwtConfig.jwkSetUri("http://localhost:8080/oauth2/jwks")
+                )
+        );
+
+        // Authorization rules
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+
+        return http.build();
+    }
+}
